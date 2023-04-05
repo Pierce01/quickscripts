0e98aaf447b6bf2e53f658be6febd0b9525c0794
Pierce <spammermuch@gmail.com>
2023-04-05 11:41:46 -0700

    createSection via API

---
+++ seattleu/main/structureGeneration/src/index.ts
@@ -0,0 +1,32 @@
+import axios from 'axios'
+import config from './config.json'
+import { ISection } from './lib/ISection'
+
+const instance = axios.create({
+  baseURL: 'https://cms.seattleu.edu/terminalfour/rs/',
+  headers: {
+    'Authorization': `Bearer ${config.token}`
+  }
+})
+
+const getSection = async (sectionID: number): Promise<ISection> => {
+  return (await instance.get(`hierarchy/${sectionID}/en`)).data
+}
+
+const createSection = async (options: ISection): Promise<ISection> => {
+  return (await instance.post(`hierarchy/en`, options)).data
+}
+
+async function main() {
+  try {
+    const newSection = await createSection({
+      name: "New Test!",
+      parent: 204148,
+    })
+    console.log(newSection)
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+main()
---
+++ seattleu/main/structureGeneration/src/lib/ISection.ts
@@ -0,0 +1,59 @@
+export interface ISection {
+  name: string,
+  parent: number,
+  status?: number,
+  workflow?: number,
+  path?: string,
+  "output-uri"?: string,
+  "access-key"?: string,
+  seo?: string,
+  "file-name"?: string,
+  show?: boolean,
+  eForm?: boolean,
+  archive?: boolean,
+  printSequence?: number,
+  contentSortMethod?: number,
+  sectionSortMethod?: number,
+  link?: boolean,
+  channels?: IChannel[],
+  userIDs?: number[],
+  inheritedUserIDs?: number[],
+  groupIDs?: number[],
+  inheritedGroupIDs?: number[],
+  viewUserIDs?: number[],
+  viewGroupIDs?: number[],
+  contentTypeScopes?: [],
+  metaDatas?: any[],
+  excludedMirrorSections?: [],
+  accessControl?: IGeneric,
+  metaData?: IGeneric,
+  pathMembers?: number[],
+  accessControlType?: number,
+  metaDataType?: number,
+  inheritedPageLayouts?: any,
+  enableOutputUri?: boolean,
+  enableOutputFilename?: boolean,
+  enablePathAsOutputUri?: boolean,
+  inheritedLinkSection?: boolean,
+  accessControlEnabled?: boolean,
+  accessControlInherited?: boolean,
+}
+
+export interface IChannel {
+  id: number,
+  pageLayout: number,
+  inheritedPageLayout: number
+}
+
+export interface IContentTypeScope {
+  id: number,
+  scope: number,
+  inherited: number
+}
+
+export interface IGeneric {
+  id: number,
+  type: number,
+  enabled: boolean,
+  active: boolean
+}
